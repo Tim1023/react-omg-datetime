daf734adcc9d4afa177c4be315366c443d13909c
Tim <timzdeveloper23@gmail.com>
2018-04-06 23:38:29 +1200

    Add onChange function

--- src/docs/index.jsx
+++ src/docs/index.jsx
@@ -2,11 +2,15 @@ import React from "react";
 import { render } from "react-dom";
 import DateTimeInput from "../../lib";
 
+const onChange = (value) => {
+  console.log(value)
+}
+
 function Demo() {
   return (
     <div>
       <h1>Demo with examples of the component</h1>
-      <DateTimeInput value={new Date(2018, 4, 4, 2, 0, 0)} />
+      <DateTimeInput value={new Date(2018, 4, 4, 2, 0, 0)} onChange={onChange} />
     </div>
   );
 }
--- src/lib/index.jsx
+++ src/lib/index.jsx
@@ -10,41 +10,53 @@ export default class DateTimeInput extends Component {
       value: new Date(),
       open: false,
       ...props,
-      labelClass:[styles.j337, styles.j332, styles.j333, styles.j336, styles.j335].join(' '),
-      displayValue:'',
+      labelClass: [styles.j337, styles.j332, styles.j333, styles.j336, styles.j335].join(' '),
+      displayValue: '',
+      tempValue: null
     }
   }
 
-  componentDidMount(){
-    const Day = this.state.value.getDate()>9?this.state.value.getDate():'0'+this.state.value.getDate()
-    const Month = this.state.value.getMonth()>9?this.state.value.getMonth():'0'+this.state.value.getMonth()
+  componentDidMount() {
+    const Day = this.state.value.getDate() > 9 ? this.state.value.getDate() : '0' + this.state.value.getDate()
+    const Month = this.state.value.getMonth() > 9 ? this.state.value.getMonth() : '0' + this.state.value.getMonth()
     const Year = this.state.value.getFullYear()
-    const Hours = this.state.value.getHours()>9?this.state.value.getHours():'0'+this.state.value.getHours()
-    const Minutes = this.state.value.getMinutes()>9?this.state.value.getMinutes():'0'+this.state.value.getMinutes()
+    const Hours = this.state.value.getHours() > 9 ? this.state.value.getHours() : '0' + this.state.value.getHours()
+    const Minutes = this.state.value.getMinutes() > 9 ? this.state.value.getMinutes() : '0' + this.state.value.getMinutes()
     this.setState({
-      displayValue:`${Day}/${Month}/${Year}, ${Hours}:${Minutes}`
+      displayValue: `${Day}/${Month}/${Year}, ${Hours}:${Minutes}`
     })
   }
 
   onOpen = () => {
+    const temp = this.state.value
     this.setState({
       open: true,
-      labelClass:[styles.j337, styles.j332, styles.j333, styles.j336, styles.j335, styles.j338].join(' '),
+      tempValue: temp,
+      labelClass: [styles.j337, styles.j332, styles.j333, styles.j336, styles.j335, styles.j338].join(' '),
     })
   }
   onClose = () => {
     this.setState({
       open: false,
-      labelClass:[styles.j337, styles.j332, styles.j333, styles.j336, styles.j335].join(' '),
+      labelClass: [styles.j337, styles.j332, styles.j333, styles.j336, styles.j335].join(' '),
     })
   }
+  trueChange = () => {
+    console.log(this.state)
+    this.state.value !== this.state.tempValue ?
+      this.props.onChange(this.state.value) : ''
+  }
+
   render() {
     return (
       <div onClick={this.onOpen} className={styles.wrap}>
         <label className={this.state.labelClass}>
           DateTimePicker</label>
         <div className={[styles.j343, styles.j344, styles.j347].join(' ')}>
-          <input className={styles.input} value={this.state.displayValue} onBlur={this.onClose}/>
+          <input className={styles.input}
+                 value={this.state.displayValue}
+                 onBlur={this.onClose}
+                 onChange={this.trueChange} />
 
         </div>
       </div>
