01ba4b61982451b7b82dc4903c2f7d2932dfd8ab
Tim <timzdeveloper23@gmail.com>
2018-04-06 21:25:03 +1200

    Initial styles

--- src/lib/index.jsx
+++ src/lib/index.jsx
@@ -10,20 +10,31 @@ export default class DateTimeInput extends Component {
       value: Date(),
       open: false,
       ...props,
+      labelClass:[styles.j337, styles.j332, styles.j333, styles.j336, styles.j335].join(' '),
     }
   }
 
   onOpen = () => {
     this.setState({
-      open: true
+      open: true,
+      labelClass:[styles.j337, styles.j332, styles.j333, styles.j336, styles.j335, styles.j338].join(' '),
     })
   }
-
+  onClose = () => {
+    this.setState({
+      open: false,
+      labelClass:[styles.j337, styles.j332, styles.j333, styles.j336, styles.j335].join(' '),
+    })
+  }
   render() {
-    console.log(styles.wrap)
     return (
-      <div onClick={this.onOpen} className={styles.wrap} >
-        <input defaultValue={this.state.value} />
+      <div onClick={this.onOpen} className={styles.wrap}>
+        <label className={this.state.labelClass}>
+          DateTimePicker</label>
+        <div className={[styles.j343, styles.j344, styles.j347].join(' ')}>
+          <input className={styles.input} defaultValue={this.state.value} onBlur={this.onClose}  />
+
+        </div>
       </div>
     )
   }
