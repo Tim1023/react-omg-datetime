a4613a8a560b9bd0645133d3a9b650b91e2d0f1a
Tim <timzdeveloper23@gmail.com>
2018-04-08 17:10:04 +1200

    Render current date

--- src/lib/Modal.jsx
+++ src/lib/Modal.jsx
@@ -28,6 +28,10 @@ export default class Modal extends React.Component {
       }
       return array_chunks(daysArray, 7)
     }
+    let rowOrder = 0;
+    const rowOrderPlus = () => {
+      rowOrder = rowOrder + 1
+    }
     return (
       <div className={styles.calendarWrap}
            tabIndex={0}
@@ -91,8 +95,28 @@ export default class Modal extends React.Component {
                   <tr role="row" className="row" key={'row' + index}>
                     {
                       row.map((col, index) => (
-                        <td role="gridcell" className={styles.calendarCell} key={'col' + index}>
-                          <div className={styles.calendarDate} aria-selected="false" aria-disabled="false">{col}</div>
+                        <td role="gridCell" className={styles.calendarCell} key={'col' + index}>
+                          {
+                            col - rowOrder > -6 && col - rowOrder < 2 ?
+                              new Date().getMonth() === this.state.value.getMonth() && new Date().getDate() === col ?
+                                <div className={styles.calendarDate + ' ' + styles.calendarToday}
+                                     aria-selected="false" aria-disabled="false">{col}</div> :
+                                <div className={styles.calendarDate} aria-selected="false"
+                                     aria-disabled="false">{col}</div> :
+                              col - rowOrder < -5
+                              && new Date().getMonth() === this.state.value.getMonth() + 1
+                              && new Date().getDate() === col ?
+                                <div className={styles.calendarDate + ' ' + styles.calendarToday}
+                                     aria-selected="false" aria-disabled="false">{col}</div> :
+                                col - rowOrder > 1
+                                && new Date().getMonth() === this.state.value.getMonth() - 1
+                                && new Date().getDate() === col ?
+                                  <div className={styles.calendarDate + ' ' + styles.calendarToday}
+                                       aria-selected="false" aria-disabled="false">{col}</div> :
+                                  <div className={styles.calendarDate + ' ' + styles.otherMonthDateColor}
+                                       aria-selected="false" aria-disabled="false">{col}</div>
+                          }
+                          {rowOrderPlus()}
                         </td>
                       ))
                     }
