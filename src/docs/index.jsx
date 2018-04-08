958e00e0b1b295b7b44f47be340b3caeb5f54d6c
Tim <timzdeveloper23@gmail.com>
2018-04-09 00:44:41 +1200

    Update demo
    Update demo

--- src/docs/index.jsx
+++ src/docs/index.jsx
@@ -12,9 +12,13 @@ function Demo() {
       <h1>Demo with examples of the component</h1>
       <DateTimeInput value={new Date(2018, 3, 4, 2, 0, 0)}
                      onChange={onChange}
-                     // seconds
+                  // seconds
       />
-      <div style={{width:500,height:500,backgroundColor:'grey'}}></div>
+      <div style={{width: 500, height: 200, backgroundColor: '#009688',color:'white',fontSize:20,padding:10,marginTop:25}}>
+        <p>In this showcase, value will be print trough console.log when you change the DataTimePicker changes.</p>
+        <p>Native Js Date object, no moment.js required.</p>
+        <p>See more on <a style={{color:'white'}} href="https://github.com/Tim1023/react-omg-datetime">Github</a></p>
+      </div>
     </div>
   );
 }
